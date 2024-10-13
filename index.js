5e013ec951ee481ea8d2b3a340f819dc49adf960
FaouzKK <121231062+FaouzKK@users.noreply.github.com>
2024-10-13 04:55:47 +0000

    version 1.0.1

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 const { tiktokdl } = require('./lib/tiktok-dl');
 
-(async () => {
-    const tiktok = await tiktokdl('https://www.tiktok.com/@.tobi.uchiha_/video/7317012349897002245?is_from_webapp=1&sender_device=pc&web_id=7408654694422414854');
-    console.log(tiktok);
-})();
+module.exports = tiktokdl ;
